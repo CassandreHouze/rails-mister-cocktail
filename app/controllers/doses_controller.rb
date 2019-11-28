2a4e908ad7e8115d528cb8f4fa18438c28191352
Cassandre Houze <cassandredelaulnoit@gmail.com>
2019-11-28 18:38:53 +0100

    commit

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,11 +8,8 @@ class DosesController < ApplicationController
     # @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     # @dose.cocktail_id = @cocktail.id
-    if @dose.save
-      redirect_to cocktail_path(params[:cocktail_id])
-    else
-      render :new
-    end
+    @dose.save
+    redirect_to cocktail_path(params[:cocktail_id])
   end
 
   def destroy
