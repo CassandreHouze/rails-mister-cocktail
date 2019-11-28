ca912f81a753e6ed0099ab989d4c9cd9d0926119
Cassandre Houze <cassandredelaulnoit@gmail.com>
2019-11-28 17:10:40 +0100

    finished code

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    # @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    # @dose.cocktail_id = @cocktail.id
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(params[:cocktail_id])
+  end
+
+  def dose_params
+    dose = params.require(:dose).permit(:description, :ingredient_id)
+    cocktail_id = params.require(:cocktail_id)
+    dose[:cocktail_id] = cocktail_id
+    dose
+  end
+end
